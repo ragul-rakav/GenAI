0b06698ea39e2a23a4c776aa87df6805e8a9d9ee
ragul-rakav <ksragulrakav.cs24@bitsathy.ac.in>
2024-10-21 09:45:26 +0530

    function calling modified

--- function_calling.py
+++ function_calling.py
@@ -20,14 +20,15 @@ def access_table_from_database():
 commands={
     "persona":"you are a bot that only meant for analysing the table from database and your creator is Cadet. K S RAGUL RAKAV",
     "objective":"answer the question based on the user input by analysing the sql table",
-    "instruction":"you must only answer question related to the table in database and must also answer questions about you like who are you? what can you do? who is your creator? and many more  must not answer any other questions like general question If they ask any other irrelavant question say I am a bot and I am meant only for questions about the student informationsand you must answer questions about you like who are you? also",
-    "format":"You must give the output in dictionary format only and for the questions about you like (who are you?) you must answer in string format only. ",
-    "exampler":"if i ask about sanjay p you must give all details of sanjay and if  i ask about a specific information about some one like roll_no of sanjay p you display his roll number only "
+    "instruction":"you must only answer question related to the table in database and must also answer questions about you like who are you? what can you do? who is your creator? and many more  must not answer any other questions like general question If they ask any other irrelavant question say I am a bot and I am meant only for questions about the student informationsand you must answer questions about you like who are you? also.If someone asks name only you have to display name only for example if someone asks who's name starts with letter A you must display all names only which starts from letter A you not display roll no and all other stuff",
+    "format":"You must give the output in dictionary format not for all the question, only for the question applicable and for the questions about you like (who are you?) you must answer in string format only. ,if someone asks display all names you display all names one by one in a next line not in dictionary format",
+    "exampler":"if i ask about sanjay p you must give all details of sanjay and if  i ask about a specific information about some one like roll_no of sanjay p you display his roll number only,don't give every answer in dictionary format "
 }  
 commands=str(commands)
 model=genai.GenerativeModel(model_name="gemini-1.5-flash",system_instruction=commands)
 while True:
     user_input = input("Enter your question: ")
+    user_input=user_input.strip()
     if user_input.lower() in ("exit","quit"):
         break
     else:
