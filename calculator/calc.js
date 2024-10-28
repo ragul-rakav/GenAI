35067140a8c3732ae7cf212137dff23acda260d5
ragul-rakav <ksragulrakav.cs24@bitsathy.ac.in>
2024-10-28 11:19:50 +0530

    modified

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,5 +1,6 @@
 let count="";
 
+
 document.getElementById("1").onclick=function(){
     count='1';
     document.getElementById("display").textContent+=count;
@@ -10,42 +11,53 @@ document.getElementById("C").onclick=function(){
 }
 
 document.getElementById("2").onclick=function(){
-    count=2
+    count='2'
     document.getElementById("display").textContent+=count;
 }
 document.getElementById("3").onclick=function(){
-    count=3
+    count='3'
     document.getElementById("display").textContent+=count;
 }
 document.getElementById("4").onclick=function(){
-    count=4
+    count='4'
     document.getElementById("display").textContent+=count;
 }
 document.getElementById("5").onclick=function(){
-    count=5
+    count='5'
     document.getElementById("display").textContent+=count;
 }
 document.getElementById("7").onclick=function(){
-    count=7
+    count='7'
     document.getElementById("display").textContent+=count;
 }
 document.getElementById("6").onclick=function(){
-    count=6
+    count='6'
     document.getElementById("display").textContent+=count;
 }
 document.getElementById("8").onclick=function(){
-    count=8
+    count='8'
     document.getElementById("display").textContent+=count;
 }
 document.getElementById("9").onclick=function(){
-    count=9
+    count='9'
     document.getElementById("display").textContent+=count;
 }
 document.getElementById("0").onclick=function(){
-    count=0    
+    count='0'   
     document.getElementById("display").textContent+=count;
 }
-// console.log(document.getElementById("display"))
+document.getElementById("+").onclick=function(){
+    document.getElementById("display").textContent+="+";
+}
+document.getElementById("-").onclick=function(){
+    document.getElementById("display").textContent+="-";
+}
+document.getElementById("*").onclick=function(){
+    document.getElementById("display").textContent+="*";
+}
+let dis=document.getElementById("display");
+
+console.log(dis.value);
 document.getElementById("CE").onclick=function(){
     count=''    
     document.getElementById("display").textContent=count;
@@ -54,6 +66,9 @@ document.getElementById("=").onclick=function(){
     if  (document.getElementById("display").textContent=='420'){
         document.getElementById("display").textContent="You are a 420";
     } 
+    else{
+        dis=eval(dis);
+    }
 
 }
 
